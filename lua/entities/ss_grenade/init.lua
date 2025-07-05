e94216bd966fe0496a69cd95595888b8dd25c3d6
TheMostUpset <themostupset@gmail.com>
2025-07-05 18:22:14 +0500

    improvements

--- lua/entities/ss_grenade/init.lua
+++ lua/entities/ss_grenade/init.lua
@@ -4,7 +4,7 @@ include("shared.lua")
 
 ENT.Model = Model("models/projectiles/serioussam/grenade.mdl")
 ENT.ExplodeOnWallHit = true -- enable explosion on wall hit
-ENT.ExplodeOnWallHitSpeed = 1000 -- min speed to explode at wall
+ENT.ExplodeOnWallHitSpeed = 1400 -- min speed to explode at wall
 
 function ENT:Initialize()
 	self:SetModel(self.Model)
@@ -30,9 +30,11 @@ end
 ENT.HitNormal = Vector(0,0,0)
 
 function ENT:PhysicsCollide(data, phys)
+	if self.didHit then return end
 	self.HitNormal = data.HitNormal
 	if self.ExplodeOnWallHit and data.HitEntity:IsWorld() and data.Speed >= self.ExplodeOnWallHitSpeed then
-		self:Explode()
+		self.didHit = true
+		self:Explode(data.HitPos, self.HitNormal)
 	else
 		if data.DeltaTime > 0.05 then self:EmitSound("weapons/serioussam/grenadelauncher/Bounce.wav") end
 		local impulse = -data.Speed * data.HitNormal * 1.5
@@ -63,7 +65,7 @@ function ENT:ExplosionEffects(pos, ang)
 	util.Effect("ss_expparticles", explosion)
 end
 
-function ENT:Explode(exppos)
+function ENT:Explode(exppos, hitnorm)
 	local pos = self:GetPos()
 	exppos = exppos or pos
 	
@@ -79,8 +81,12 @@ function ENT:Explode(exppos)
 
 	self:ExplosionEffects(pos, self.HitNormal:Angle())
 	
-	local trace = util.TraceLine({start = self:GetPos() + Vector(0,2,0), endpos = self:GetPos() - Vector(0,0,32), filter=self})
-	util.Decal("Scorch", trace.HitPos + trace.HitNormal, trace.HitPos - trace.HitNormal)
+	if hitnorm then
+		util.Decal("Scorch", pos + hitnorm, pos - hitnorm)
+	else
+		local trace = util.TraceLine({start = self:GetPos() + Vector(0,2,0), endpos = self:GetPos() - Vector(0,0,32), filter=self})
+		util.Decal("Scorch", trace.HitPos + trace.HitNormal, trace.HitPos - trace.HitNormal)
+	end
 	
 	self:EmitSound("weapons/serioussam/Explosion02.wav", 100, 100)
 	local owner = self:GetOwner()
