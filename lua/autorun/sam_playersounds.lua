718ca6a5389abbd521b896f371d1f9b086ee6faa
TheMostUpset <themostupset@gmail.com>
2025-07-05 19:23:38 +0500

    add more models for player sounds

--- lua/autorun/sam_playersounds.lua
+++ lua/autorun/sam_playersounds.lua
@@ -1,23 +1,33 @@
+SeriousPlayerSounds = {}
+
 local cvar_plysounds = CreateConVar("ss_playersounds", 1, {FCVAR_ARCHIVE, FCVAR_REPLICATED})
 
-local sam_models = {
+SeriousPlayerSounds.ModelTable = {
 	["models/pechenko_121/samclassic.mdl"] = true,
 	["models/pechenko_121/beheadedben.mdl"] = true,
 	["models/pechenko_121/redrick.mdl"] = true,
-	["models/pechenko_121/steelsteve.mdl"] = true
+	["models/pechenko_121/steelsteve.mdl"] = true,
+	["models/pechenko_121/dancingden.mdl"] = true,
+	["models/pechenko_121/hillyharry.mdl"] = true,
+	["models/pechenko_121/kleerkurt.mdl"] = true,
+	["models/pechenko_121/samclassic_mental.mdl"] = true,
+	["models/pechenko_121/samclassic_pirate.mdl"] = true,
+	["models/pechenko_121/samclassic_santa.mdl"] = true,
+	["models/pechenko_121/samclassic_tfe.mdl"] = true,
+	["models/pechenko_121/skinlessstan.mdl"] = true
 }
 
-local function IsPlayerSoundsEnabled()
+function SeriousPlayerSounds:IsEnabled()
 	return cvar_plysounds:GetBool()
 end
 
-local function PlayerHasSeriousSamModel(ply)
-	return sam_models[ply:GetModel()]
+function SeriousPlayerSounds:PlayerUsesModel(ply)
+	return SeriousPlayerSounds.ModelTable[ply:GetModel()]
 end
 
-hook.Add("OnPlayerJump", "SeriousSamJumpSound", function(ply)
-	if !IsPlayerSoundsEnabled() then return end
-	if PlayerHasSeriousSamModel(ply) then
+hook.Add("OnPlayerJump", "SeriousPlayerSounds_Jump", function(ply)
+	if !SeriousPlayerSounds:IsEnabled() then return end
+	if SeriousPlayerSounds:PlayerUsesModel(ply) then
 		local filter = nil
 		if SERVER and !game.SinglePlayer() then
 			filter = RecipientFilter()
@@ -33,9 +43,9 @@ end)
 
 if CLIENT then return end
 
-hook.Add("PlayerHurt", "samplayerhurt", function(ply, at, h, dmg)	
-	if !IsPlayerSoundsEnabled() then return end
-	if PlayerHasSeriousSamModel(ply) and ply:Health() > 0 then
+hook.Add("PlayerHurt", "SeriousPlayerSounds_Hurt", function(ply, at, h, dmg)	
+	if !SeriousPlayerSounds:IsEnabled() then return end
+	if SeriousPlayerSounds:PlayerUsesModel(ply) and ply:Health() > 0 then
 		if !ply.SSHurtSoundDelay or ply.SSHurtSoundDelay <= CurTime() then
 			local pitch = math.random(92,112)
 			if ply:WaterLevel() == 3 then
@@ -52,9 +62,9 @@ hook.Add("PlayerHurt", "samplayerhurt", function(ply, at, h, dmg)
 	end
 end)
 
-hook.Add("PlayerDeath", "samdeath", function(ply)
-	if !IsPlayerSoundsEnabled() then return end
-	if PlayerHasSeriousSamModel(ply) then
+hook.Add("PlayerDeath", "SeriousPlayerSounds_Death", function(ply)
+	if !SeriousPlayerSounds:IsEnabled() then return end
+	if SeriousPlayerSounds:PlayerUsesModel(ply) then
 		if ply:WaterLevel() == 3 then
 			ply:EmitSound("player/serioussam/DeathWater.wav", 70, 100)
 		elseif ply:WaterLevel() < 3 then
