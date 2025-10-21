c33495007452af982f66f31480a7c036680b6b92
TheMostUpset <themostupset@gmail.com>
2025-10-21 16:52:09 +0500

    update rocket trail

--- lua/entities/ss_rocket/cl_init.lua
+++ lua/entities/ss_rocket/cl_init.lua
@@ -1,5 +1,31 @@
 include('shared.lua')
 
+function ENT:Initialize()
+	self.emitter = ParticleEmitter(self:GetPos())
+end
+
 function ENT:Draw()
 	self:DrawModel()
+
+	if self:WaterLevel() >= 2 or self:GetVelocity():Length() < 100 or (CurTime() - self:GetCreationTime()) < .1 then return end
+	
+	local particle = self.emitter:Add("particle/particle_smokegrenade", self:GetPos())
+	if particle then
+		particle:SetVelocity(VectorRand()*5)
+		particle:SetAirResistance(0)
+		particle:SetDieTime(.5)
+		particle:SetStartAlpha(75)
+		particle:SetEndAlpha(0)
+		particle:SetStartSize(2.5)
+		particle:SetEndSize(25)
+		particle:SetRoll(math.Rand(-90, 90))
+		particle:SetRollDelta(math.Rand(-.25, .25))
+		particle:SetColor(255, 255, 255)
+	end
+end
+
+function ENT:OnRemove()
+	if self.emitter and self.emitter:IsValid() then
+		self.emitter:Finish()
+	end
 end
--- lua/entities/ss_rocket/init.lua
+++ lua/entities/ss_rocket/init.lua
@@ -4,6 +4,8 @@ include("shared.lua")
 
 ENT.Model = Model("models/projectiles/serioussam/rocket.mdl")
 ENT.FlySound = Sound("weapons/serioussam/RocketFly.wav")
+ENT.TrailTexture = "trails/laser"
+ENT.TrailLifeTime = .8
 
 function ENT:Initialize()
 	self:SetModel(self.Model)
@@ -12,7 +14,8 @@ function ENT:Initialize()
 	self:SetCollisionBounds(Vector(), Vector())
 	self:SetCollisionGroup(COLLISION_GROUP_PROJECTILE)
 
-	local glow = ents.Create("env_sprite")
+	self.glow = ents.Create("env_sprite")
+	local glow = self.glow
 	glow:SetKeyValue("rendercolor","255 180 60")
 	glow:SetKeyValue("GlowProxySize","2")
 	glow:SetKeyValue("HDRColorScale","1")
@@ -26,7 +29,8 @@ function ENT:Initialize()
 	glow:SetPos(self:GetPos())
 	
 	self:EmitSound(self.FlySound)
-	ParticleEffectAttach("rocket_smoke_trail", PATTACH_ABSORIGIN_FOLLOW, self, 0)
+	-- ParticleEffectAttach("rocket_smoke_trail", PATTACH_ABSORIGIN_FOLLOW, self, 0)
+	util.SpriteTrail(self, 0, Color(200, 200, 200), true, 50, 2.5, self.TrailLifeTime, 1 / ( 15 + 1 ) * 0.5, self.TrailTexture)
 end
 
 function ENT:Explode(pos, norm, hitEnt)
@@ -37,7 +41,14 @@ function ENT:Explode(pos, norm, hitEnt)
 	self:DoSplashDamage(pos, self.DamageSplash, owner)
 	self:DoDirectDamage(hitEnt, self.DamageDirect, owner)
 	
-	self:Remove()
+	-- fake remove
+	self:OnRemove()
+	self:SetLocalVelocity(Vector())
+	self:SetMoveType(MOVETYPE_NONE)
+	self:AddEffects(EF_NODRAW)
+	self:AddSolidFlags(FSOLID_NOT_SOLID)
+	if self.glow and IsValid(self.glow) then self.glow:Remove() end
+	SafeRemoveEntityDelayed(self, self.TrailLifeTime)
 end
 
 function ENT:Touch(ent)
@@ -57,6 +68,7 @@ function ENT:OnRemove()
 end
 
 function ENT:Think()
+	if self.didHit then return end
 	self:SetLocalVelocity(self:GetForward() * 1024)
 end
 
